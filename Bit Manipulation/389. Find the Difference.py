c0114d294c22e2f8abf0cf5f912eb42cc07f0d2d
bennbenben <80408902+bennbenben@users.noreply.github.com>
2022-02-08 03:58:59 +0800

    Delete Bit Manipulation directory

--- Bit Manipulation/389. Find the Difference.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        utc_s_sum=utc_t_sum=0
-        for i in s:
-            utc_s_sum += ord(i)
-        for i in t:
-            utc_t_sum += ord(i)
-        return chr(utc_t_sum-utc_s_sum)
