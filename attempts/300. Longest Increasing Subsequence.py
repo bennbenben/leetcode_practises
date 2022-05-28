629e95c9b5fec94961823eb3b57edb0db3fe2f8a
bennbenben <80408902+bennbenben@users.noreply.github.com>
2022-05-28 21:09:07 +0800

    Update 300. Longest Increasing Subsequence.py

--- attempts/300. Longest Increasing Subsequence.py
+++ attempts/300. Longest Increasing Subsequence.py
@@ -1,6 +1,15 @@
 from typing import List
 
 class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        lis = [1]*len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    lis[i] = max(lis[i],lis[j]+1)
+        return max(lis)
+
+class Solution:
 	def lengthOfLis(self, nums: List[int]) -> int:
 		lis = [1] * len(nums)
 
