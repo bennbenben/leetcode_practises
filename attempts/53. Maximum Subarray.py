8c627e151763cd0b612ec5d8442518f0b6e44157
bennbenben <80408902+bennbenben@users.noreply.github.com>
2022-05-30 01:32:06 +0800

    Update 53. Maximum Subarray.py

--- attempts/53. Maximum Subarray.py
+++ attempts/53. Maximum Subarray.py
@@ -1,5 +1,25 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        curr_sum = max_sum = nums[0]
+        for i in range(1,len(nums)):
+            curr_sum = max(curr_sum+nums[i], nums[i])
+            max_sum = max(curr_sum, max_sum)
+        return max_sum
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr_sum = max_sum = nums[0]
+        for i in range(1,len(nums)):
+            curr_sum += nums[i]
+            
+            if curr_sum < nums[i]:
+                curr_sum = nums[i]
+                
+            max_sum = max(curr_sum, max_sum)
+        return max_sum
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
         max_sum, curr_sum = nums[0],0
         
         for n in nums:
