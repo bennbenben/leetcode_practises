de8e006bc275956dac8a6cb54cda8d2c09e62311
Ben Loy <bennbenbenl@gmail.com>
2022-06-07 01:20:00 +0800

    445. Add Two Numbers II

---
+++ attempts/445. Add Two Numbers II.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        list_1, list_2 = list(), list()
+        
+        while l1:
+            list_1.append(str(l1.val))
+            l1 = l1.next
+        while l2:
+            list_2.append(str(l2.val))
+            l2 = l2.next
+        
+        val_1, val_2 = int("".join(list_1)), int("".join(list_2))
+        output_list = list(str(val_1+val_2))
+        
+        sentinel_head = ListNode(0)
+        prev_node = sentinel_head
+        for head in output_list:
+            new_node = ListNode(head)
+            prev_node.next = new_node
+            prev_node = new_node
+        
+        return sentinel_head.next
